75c3afc6b659e146046aee263d1ae8ff29f7340e
korben.dong <ghostyuan@outlook.com>
2025-01-11 20:56:25 +0800

    update gdb dashboard

--- gdb/init.sh
+++ gdb/init.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -f
 
+git clone git@github.com:cyrus-and/gdb-dashboard.git
 mkdir -p ~/.config/gdb
-cp gdbinit ~/.config/gdb
+cp gdb-dashboard/.gdbinit ~/.config/gdb/gdbinit
+
+mkdir -p ~/.gdbinit.d
+cp init ~/.gdbinit.d
