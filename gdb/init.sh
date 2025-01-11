94a8904b99889f29d8785221b908b52479f24c9c
korben.dong <ghostyuan@outlook.com>
2025-01-11 23:47:46 +0800

    update gdb

--- gdb/init.sh
+++ gdb/init.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -f
 
-git clone git@github.com:cyrus-and/gdb-dashboard.git
+if [ ! -d "gdb-dashboard" ]; then
+  git clone git@github.com:cyrus-and/gdb-dashboard.git
+fi
 mkdir -p ~/.config/gdb
+git -C gdb-dashboard pull origin master
 cp gdb-dashboard/.gdbinit ~/.config/gdb/gdbinit
 
 mkdir -p ~/.gdbinit.d
